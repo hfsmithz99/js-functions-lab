1f65fa014083265e737ab82c2ae773a994938f99
Henry Smith <h.f.smithz99@gmail.com>
2023-09-27 15:31:21 -0400

    function and template literal practice

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log("Hello");
+/*defining a function
+//declaring a function
+//creating the reusable block of code
+//to be used at a later time in the program
+function sayHello(personsName){
+    //write the code to say hello here
+    console.log(`Hello ${personsName}`);
+}
+
+//execute the function
+//invoke the function
+//call the function
+
+//write the functions name ()
+sayHello('Shavon'); //runs the block of code defined by the sayHello func
+*/
+function sayGoodbye(phrase, personsName){
+    console.log(`${phrase}, ${personsName}`)
+}
+
+sayGoodbye('Adios', 'Laura');
+
